58a82e3dd5165380d96c43ce9d9f33be04c98b6a
273vibhuti@gmail.com <273vibhuti@gmail.com>
2026-05-27 17:55:34 +0530

    uplode

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import ProductForm from "./components/ProductForm";
+import PrivateRoute from "./components/PrivateRoute";
 
-function App() {
+function Login() 
+{
+  const login = () => 
+  {
+    localStorage.setItem("auth", "true");
+    window.location.href = "/";
+  };
+  return 
+  (
+    <div className="container mt-5">
+      <h2>Sign In</h2>
+      <button className="btn btn-primary" onClick={login}>Sign In</button>
+    </div>
+  );
+}
+export default function App() 
+{
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<ProductList />}/>
+        <Route path="/add" element={<ProductForm />}/>
+      </Routes>
     </>
-  )
+  );
 }
-
-export default App
-
